61d1a21d0b32d3f7384e821eccd0021bad176edb
Pranshu1806 <pranshu.mishra3118@gmail.com>
2022-05-07 08:18:07 +0530

    first commit

---
+++ forloop.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class forloop {
+    public static void main(String[] args) {
+
+
+
+    for(int i=1;i<=9;i++) {
+        for (int a = 0; a <= i; a++) {
+            System.out.println(a);
+        }
+    }
+}}
