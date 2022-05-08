642657c1cadc3e1a2c01c33e5fe0c0377150e376
Pranshu1806 <pranshu.mishra3118@gmail.com>
2022-05-08 14:31:38 +0530

    First commit by Master Branch

---
+++ loops.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class loops {
+    public static void main(String[] args) {
+
+
+         int n =1;
+        while (n <= 3) {
+            System.out.println(n);
+            n++;
+        }
+        System.out.println("Finishing loop");
+
+    }
+}
